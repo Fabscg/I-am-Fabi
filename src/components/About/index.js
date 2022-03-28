97f8279232f6d068e2811a8d76579e6ac6205931
Fabulosa <fabiscg79@gmail.com>
2022-03-27 23:04:47 -0400

    about page

---
+++ src/components/About/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import avatar from '../../assets/profile/Fabs.jpg'
+
+
+function About(props) {
+    return (
+        <div class="w3-container w3-dark-grey w3-center w3-text-light-grey w3-padding-32" id="about">
+            <h4><b>About Me</b></h4>
+            <img src={avatar} alt="Me" class="w3-image w3-padding-32" style={{width:600}}/>
+            <div class="w3-content w3-justify" style={{width:600}}>
+                <h4>I am Fabi</h4>
+                <p>
+                    People find me to be an upbeat self-motivated team player with excellent
+                    communication skills. For the past several years, I have worked in customer service and hospitality, and
+                    have also spearheaded my own business. Having held a lifelong affinity for coding and programming, I
+                    endeavoured to equip myself with the knowledge and skillset to pursue a career as a developer. Over the
+                    last two
+                    years, I independently dedicated myself to deepening my studies and experience in web development and
+                    acquired
+                    proficiency in the breadth of programming languages. I am a curious and driven individual, and excited
+                    for an opportunity to specialize in this dynamic industry.
+                </p>
+
+                <p>mail: fabiscg79@gmail.com</p>
+                <p>tel: 647-888-4713</p>
+            </div>
+        </div>
+
+    )
+}
+
+
+export default About;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,29 +1,38 @@
 
 import React from 'react'
 
-function Nav() {
+function Nav({ currentPage, handlePageChange }) {
     return (
         <div>
             <div className="w3-top">
+
                 <div className="w3-bar w3-card" id="myNavbar">
-                    <a href="#home" className="title w3-bar-item w3-button w3-wide"><i class="fa-solid fa-ankh"></i> I am Fabi</a>
+                    <a href="#home" className="title w3-bar-item w3-button w3-wide"><i class="fa-solid fa-laptop-code"></i> I am Fabi</a>
 
                     <div className="w3-right w3-hide-small">
-                        <a href="#Home" className="w3-bar-item w3-button">HOME</a>
-                        <a href="#About" className="w3-bar-item w3-button"><i className="fa fa-user"></i>ABOUT</a>
-                        <a href="#Portfolio" className="w3-bar-item w3-button"><i className="fa fa-th"></i> PORTFOLIO</a>
-                        <a href="#contact" className="w3-bar-item w3-button"><i className="fa fa-envelope"></i> CONTACT</a>
+                        <a href="#home" className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+
+                            onClick={() => handlePageChange('Home')}>HOME</a>
+                        <a href="#about" className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+
+                            onClick={() => handlePageChange('About')}><i className="fa fa-user"></i>ABOUT</a>
+                        <a href="#portfolio" className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+
+                            onClick={() => handlePageChange('Portfolio')}><i className="fa fa-th"></i> PORTFOLIO</a>
+                        <a href="#contact" className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+
+                            onClick={() => handlePageChange('Contact')}><i className="fa fa-envelope"></i> CONTACT</a>
                     </div>
 
-                    <a href="project" className="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="w3_open()">
+                    <a href="project" className="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onClick="w3_open()">
                         <i className="fa fa-bars"></i>
                     </a>
                 </div>
             </div>
 
 
-         
-            <nav className="w3-sidebar w3-bar-block w3-black w3-card w3-animate-left w3-hide-medium w3-hide-large" style={{display:"none"}} id="mySidebar">
+
+            <nav className="w3-sidebar w3-bar-block w3-black w3-card w3-animate-left w3-hide-medium w3-hide-large" style={{ display: "none" }} id="mySidebar">
                 <a href="project" onClick="w3_close()" className="w3-bar-item w3-button w3-large w3-padding-16">Close ×</a>
                 <a href="#home" onclick="w3_close()" className="w3-bar-item w3-button">HOME</a>
 
