cbeae5dbdea9b691ce420eebed6680a37f26a7aa
Fabulosa <fabiscg79@gmail.com>
2022-05-26 14:46:56 -0400

    nothing to commit

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 const express = require("express")
 
-const server = express()
+const server = express({ server })
     .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 const { Server } = require('wss');
 
-const ws = new Server({ server });
-ws.on('connection', (ws) => {
+const wss = new Server({ server });
+wss.on('connection', (wss) => {
     console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
+    wss.on('close', () => console.log('Client disconnected'));
 });
