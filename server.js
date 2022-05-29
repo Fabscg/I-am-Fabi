0dc2abc34222581aa76a46bff01a28a2fee930f5
Fabulosa <fabiscg79@gmail.com>
2022-05-29 13:46:55 -0400

    fixing socket

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 const express = require("express")
 
-const server = express({ server })
+const server = express()
     .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 const { Server } = require('wss');
