b96ebbcb7ccdd0ad7fd97bc720c75c1ee62aa516
Fabiola Castaneda <fabiscg79@gmail.com>
2023-11-30 18:03:59 -0500

    nothing to commit

--- server.js
+++ server.js
@@ -12,3 +12,21 @@ wss.on('connection', (wss) => {
     console.log('Client connected');
     wss.on('close', () => console.log('Client disconnected'));
 });
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDck-I1Ak9cbCg-s_6aRa5aTzH447V93QY",
+  authDomain: "i-am-fabi.firebaseapp.com",
+  databaseURL: "https://i-am-fabi-default-rtdb.firebaseio.com",
+  projectId: "i-am-fabi",
+  storageBucket: "i-am-fabi.appspot.com",
+  messagingSenderId: "361124503866",
+  appId: "1:361124503866:web:e0f64c6e3da6f236ff3003"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
